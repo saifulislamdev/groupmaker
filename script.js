86f80f0f7633435d27be20892c0ae2aa9c798161
Saiful Islam <nysaifulislam@gmail.com>
2020-05-26 21:31:22 -0400

    improvements to space complexity

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 function groupMaker(num) {
     var array = arrayMaker(); // input array created from user input
     var groupArray = []; // collection of random groups will be stored in this array
-    var tempRandGroup = []; // current random group to be inserted into groupArray (temporary group placeholder)
-    var randIndex, insertInTemp;
-    var numOfGroups = array.length / num; // how many full groups can be created
-    var counter = 0; // keeps track of how many names in placeholder
+    var randIndex;
+    var numOfNames = array.length;
     if (checkForEmptiness(array, num)) { // if check makes sure user input has at least 'num' entries (i.e. user must input at least 2 names for groups of 2)
-        for (var i = 0; i < numOfGroups; i++) { // creates random groups one at a time
-            while (counter != num) {
-                randIndex = randomNum(array.length); // current random element index
-                insertInTemp = array[randIndex]; // current random element
-                array.splice(randIndex, 1); // after making a copy of random element, delete random element from array
-                tempRandGroup.push(insertInTemp); // push copy of current random element into temporary group placeholder
-                counter++;
-            }
-            counter = 0;
-            groupArray.push(tempRandGroup); // push current random group in placeholder to collection of random groups
-            tempRandGroup = []; // reset temporary group placeholder
-        }
-        if ((array.length) % num != 0) { // push final random group if number of names from user input and number of names in each group are not divisible (names are leftover)
-            groupArray.push(pushFinalArray(array, num));
+        for (var i = 0; i < numOfNames; i++) { 
+            randIndex = randomNum(array.length); // current random element index
+            groupArray.push(array[randIndex]); // push current random element into collection of random groups
+            array.splice(randIndex, 1); // after making a copy of random element, delete random element from array
         }
         printGroups(groupArray, num); // prints random groups
     } else { // if number of names entered < pairs of groups (i.e. if there is only 2 names entered by user for groups of 3)
@@ -27,40 +15,39 @@ function groupMaker(num) {
     }
 }
 
-function printGroups(array) {
+function printGroups(array, num) {
     var string = "";
-    var tempString = "";
-    for (var i = 0; i < array.length; i++) { // print groups one at a time
-        for (var j = 0; j < array[i].length; j++) {
-            if (j == 0) { // first name in group
-                if (typeof array[i][j + 1] == 'undefined') { // if the first name is the only name in the group
-                    tempString = "<p>GROUP " + (i + 1) + ": " + array[i][j];
+    var currGroup = 1;
+    for (var i = 1; i <= array.length; i = i + num) { 
+        var startInd = i - 1;
+        for (var j = startInd; j < startInd + num; j++) {
+            if (j == startInd) { // first name in group
+                if (typeof array[j + 1] == 'undefined' || num == 1) { // if the first name is the only name in the group
+                    string = string.concat("<p>GROUP " + currGroup + ": " + array[j]);
                     break;
                 } else {
-                    tempString = "<p>GROUP " + (i + 1) + ": " + array[i][j] + ", ";
+                    string = string.concat("<p>GROUP " + currGroup + ": " + array[j] + ", ");
                 }
-            } else if (typeof array[i][j + 1] == 'undefined') { // last name in the group
-                tempString = tempString.concat(array[i][j] + "\n");
+            } else if (j == startInd + num - 1 || typeof array[j + 1] == 'undefined') { // last name in the group
+                string = string.concat(array[j] + "\n");
                 break;
             } else { // not the first or last name in the group
-                tempString = tempString.concat(array[i][j] + ", ");
+                string = string.concat(array[j] + ", ");
             }
         }
-        string = string.concat(tempString); // add current group to output
-        tempString = ""; // reset temporary string placeholder to add next group
+        currGroup++;
     }
-    document.getElementById("output").innerHTML = string;
+    document.getElementById("output").innerHTML = string; 
 }
 
 function arrayMaker() {
-    var inputString = document.getElementById("input").value; // accesses input
+    var inputString = document.getElementById("input").value; 
     if (inputString == "") {
         document.getElementById("output").innerHTML = "NO NAMES ARE ENTERED. ENTER NAMES AND TRY AGAIN.";
     } else {
         inputString = inputString.toUpperCase();
-        inputString = removeExtraCommas(inputString); // removes extra commas accidentally inserted by user
-        var inputArr = inputString.split(','); // creates input array from input string
-        return inputArr;
+        inputString = removeExtraCharacters(inputString); // removes extra characters accidentally inserted by user
+        return inputString.split(','); 
     }
 }
 
@@ -69,11 +56,11 @@ function moreOption() {
     if (namesPerGroup == "") {
         document.getElementById("output").innerHTML = "PLEASE SPECIFY NUMBER OF PEOPLE PER GROUP (ENTER NUMBER ONLY)";
     } else {
-        groupMaker(namesPerGroup);
+        groupMaker(parseInt(namesPerGroup));
     }
 }
 
-function checkForEmptiness(array, num) {
+function checkForEmptiness(array, num) { // makes sure user input has at least 'num' entries
     for (var i = 0; i < num; i++) {
         if (array[i] == "" || typeof array[i] == 'undefined') {
             return false;
@@ -82,25 +69,15 @@ function checkForEmptiness(array, num) {
     return true;
 }
 
-function pushFinalArray(array, num) {
-    var finalArray = [];
-    for (var i = 1; i < num; i++) {
-        if (typeof array[i - 1] != 'undefined') {
-            finalArray.push(array[i - 1]);
-        }
-    }
-    return finalArray;
-}
-
-function removeExtraCommas(string) {
+function removeExtraCharacters(string) { // removes extra characters accidentally inserted by user
     while (string[0] == ',' || string[0] == " ") { // remove comma or space at beginning (if any)
         string = string.slice(1, string.length);
     }
-    while (string[string.length - 1] == ',' || string[0] == " ") { // remove comma or space at end (if any)
+    while (string[string.length - 1] == ',' || string[string.length - 1] == " ") { // remove comma or space at end (if any)
         string = string.slice(0, string.length - 1);
     }
-    for (var i = 0; i < string.length; i++) {
-        while (string[i] == " " && string[i - 1] == " ") { // remove extra spaces
+    for (var i = 1; i < string.length; i++) {
+        while (string[i] == " " && string[i - 1] == " ") { // remove consecutive spaces
             string = string.slice(0, i) + string.slice(i + 1, string.length);
         }
         while (string[i] == ',' && string[i - 1] == ',') { // remove consecutive commas
